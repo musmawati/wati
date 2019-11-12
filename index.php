3cfba24817444f863af21c41cadbe8537afe3885
musmawati <57651010+musmawati@users.noreply.github.com>
2019-11-12 12:46:58 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,85 @@
+
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" href="index.css">
+	</head>
+	<body>
+		<div class="header">
+			<img src="logo.jpg">
+			<h1>wati_SHOP.COM</h1>
+			<H5>belanja online hanya disini, barang berkualitas, aman & terpercaya</H5>
+		</div>
+
+
+
+		<div class="menu">
+			<ul>
+				<li>Halaman Depan</li>
+				<li>Informasi</li>
+				<li>Kontak</li>
+				<a href="daftar.php"><li>Daftar</li></a>
+				<a href="login.php"><li>Login</li></a>
+			</ul>
+		</div>
+
+
+
+		<div class="content">
+			
+
+			<div class="bagiankiri">
+				
+				<h1 font size="30" align="center">Daftar Produk</h1>
+
+				<div class="produk">
+					<h4>TAS</h4>
+					<p>Harga : Rp.200,-</p>
+					<p>Stok 5</p>
+					<img src="tas.jpg" alt="">
+					
+				</div>
+
+				<div class="produk">
+					<h4>SEPATU</h4>
+					<p>Harga : Rp. 500,-</p>
+					<p>Stok 50</p>
+					<img src="sepatu.jpg" alt="">
+				</div>
+
+				<div class="produk">
+					<h4>BAJU GAMIS</h4>
+					<p>Harga : Rp. 800.000,-</p>
+					<p>Stok 30</p>
+					<img src="gamis.jpg"alt="">
+				</div>
+
+				<div class="produk">
+					<h4>SANDAL</h4>
+					<p>Harga : Rp. 550.000,-</p>
+					<p>Stok 10</p>
+					<img src="sandal.jpg" alt="">
+				</div>
+
+				
+			<div class="bagiankanan">
+				<p><h1>Iklan</p></h1><hr>
+				<hr><br>
+				<img src="makanan.jpg" width="100" height="150"><br><a href="tipmakan.php"><h3>Klik disini Makanan khas padang Jam</a></h3>
+				<br><hr>
+				<br>
+				
+
+				<img src="minuman.jpg" width="100" height="150"><br><a href="tipminuman.php"><h3>Minuman segar</a></h3>
+				<br>
+				<br>
+				<br>
+
+			</div>
+
+		</div>
+
+<p align="center">=================================================<br>PADANG, OKTOBER 2019 <br> Programer by : Musmawati<br>=================================================</p>
+	</body>
+</html>
+
